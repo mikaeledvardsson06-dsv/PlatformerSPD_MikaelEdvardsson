0cc4b70362bf58c84b58d5f6001ab7b651d1c25c
NirrePirre <noelswe11@gmail.com>
2025-09-23 20:25:12 +0200

    FireTrap version 1
    I made i fire trap that is not fully finished.

---
+++ Assets/Scripts/FireTrap.cs
@@ -0,0 +1,86 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections;
+
+public class Firetrap : MonoBehaviour
+{
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    [SerializeField] private int damageAmount = 1;
+    [SerializeField] private float damageCooldown = 1f;
+
+    private bool triggered = false;
+    private bool active = false;
+    private float lastDamageTime;
+
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!triggered)
+            {
+                triggered = true;
+                StartCoroutine(ActiveTrap());
+            }
+
+            if (active)
+            {
+                PlayerMovement health = other.GetComponent<PlayerMovement>();
+                if (health != null)
+                {
+                    health.TakeDamage(damageAmount);
+                }
+            }
+        }
+    }
+
+    private IEnumerator ActiveTrap()
+    {
+        if (anim != null)
+        {
+            anim.SetTrigger("Extend");
+        }
+
+        yield return new WaitForSeconds(activationDelay);
+
+        active = true;
+
+        if (anim != null)
+        {
+            anim.SetBool("TrapActive", true);
+        }
+
+        yield return new WaitForSeconds(activeTime);
+
+        active = false;
+        if (anim != null)
+        {
+            anim.SetBool("TrapActive", false);
+        }   
+
+        triggered = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && active)
+        {
+            PlayerMovement health = other.GetComponent<PlayerMovement>();
+            if (health != null && Time.time >= lastDamageTime + damageCooldown)
+            {
+                health.TakeDamage(damageAmount);
+                lastDamageTime = Time.time;
+            }
+        }
+    }
+}   
